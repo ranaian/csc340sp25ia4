7fadbf408b6552a4e45a831c831422b8daaff796
Ranaian <vbnahser@gmail.com>
2025-04-09 23:23:30 -0400

    initial commit of mvc controller update, 0% working

--- src/main/java/com/crudapi/demo/BirdController.java
+++ src/main/java/com/crudapi/demo/BirdController.java
@@ -3,9 +3,11 @@ package com.crudapi.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @RequestMapping("/birds")
 public class BirdController {
 
@@ -18,8 +20,11 @@ public class BirdController {
      * @return a list of bird objects
      */
     @GetMapping("/all")
-    public Object getAllBirds() {
-        return new ResponseEntity<>(birdService.getAllBirds(), HttpStatus.OK);
+    public Object getAllBirds(Model model) {
+        //return new ResponseEntity<>(birdService.getAllBirds(), HttpStatus.OK);
+        model.addAttribute("birdList", birdService.getAllBirds());
+        model.addAttribute("title", "All Birds");
+        return "bird-list";
     }
 
     /**
@@ -30,8 +35,11 @@ public class BirdController {
      * @return one bird object
      */
     @GetMapping("/{birdId}")
-    public Object getOneBird(@PathVariable int birdId){
-        return new ResponseEntity<>(birdService.getBirdById(birdId), HttpStatus.OK);
+    public Object getOneBird(@PathVariable int birdId, Model model){
+        //return new ResponseEntity<>(birdService.getBirdById(birdId), HttpStatus.OK);
+        model.addAttribute("bird", birdService.getBirdById(birdId));
+        model.addAttribute("title" , "Bird #: " + birdId);
+        return "bird-details";
     }
 
     /**
@@ -41,8 +49,11 @@ public class BirdController {
      * @return a list of bird objects matching the family
      */
     @GetMapping("/family/{family}")
-    public Object getBirdsByFamily(@PathVariable String family){
-        return new ResponseEntity<>(birdService.getBirdsByFamily(family), HttpStatus.OK);
+    public Object getBirdsByFamily(@PathVariable String family, Model model){
+        //return new ResponseEntity<>(birdService.getBirdsByFamily(family), HttpStatus.OK);
+        model.addAttribute("birdList", birdService.getBirdsByFamily(family));
+        model.addAttribute("title" , "Birds by Family: " + family);
+        return "bird-list";
     }
     /**
      * Get a list of birds with names that contain the given string
@@ -51,10 +62,14 @@ public class BirdController {
      * @return list of bird objects matching search key
      * (not working)
      */
-        @GetMapping("/name")
-        public Object getBirdsByName(@RequestParam(name = "search", defaultValue = "") String search){
-            return new ResponseEntity<>(birdService.getBirdsByName(search), HttpStatus.OK);
-        }
+    @GetMapping("/name")
+    public Object getBirdsByName(@RequestParam(name = "search", defaultValue = "") String search, Model model){
+        //return new ResponseEntity<>(birdService.getBirdsByName(search), HttpStatus.OK);
+        model.addAttribute("birdList", birdService.getBirdsByName(search));
+        model.addAttribute("title", "Birds by Name: " + search);
+        return "bird-list";
+    }
+
 
 
     /**
@@ -68,9 +83,9 @@ public class BirdController {
      * @return the updated list of birds
      */
     @PostMapping("/new")
-    public Object addNewBird(@RequestBody Bird newBird) {
+    public Object addNewBird(Bird newBird, Model model) {
         birdService.addNewBird(newBird);
-        return new ResponseEntity<>(birdService.getAllBirds(), HttpStatus.OK);
+        return "redirect:/birds/all";
     }
 
     /**
@@ -86,15 +101,18 @@ public class BirdController {
      * @return the updated bird object
      */
     @PutMapping("/update/{birdId}")
-    public Object updateBird(@PathVariable int birdId, @RequestBody Bird bird){
-        birdService.updateBird(birdId, bird);
-        return new ResponseEntity<>(birdService.getBirdById(birdId), HttpStatus.CREATED);
+    public Object updateBird(@PathVariable int birdId, Model model){
+        //birdService.updateBird(birdId, bird);
+        model.addAttribute("bird", birdService.getBirdById(birdId));
+        //model.addAttribute("")
+        return "bird-update";
     }
 
     @DeleteMapping("/delete/{birdId}")
     public Object deleteBirdById(@PathVariable int birdId) {
         birdService.deleteBirdById(birdId);
-        return new ResponseEntity<>(birdService.getAllBirds(), HttpStatus.OK);
+        //return new ResponseEntity<>(birdService.getAllBirds(), HttpStatus.OK);
+        return "redirect:/birds/all";
     }
 
 }
